c7e9e27a2a58d9d58b8af134595c1fa5fbcbcc22
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-08-20 21:41:48 +0300

    Implement ToyService with LocalStorage

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -20,6 +20,32 @@ function query(filterBy = {}) {
             }
             return toys
         })
+}
 
+function getById(toyId) {
+    return storageService.get(TOY_KEY, toyId)
+}
 
+function save(toy) {
+    const method = bug._id ? 'put' : 'post'
+    return storageService[method](TOY_KEY, toy)
+}
+
+function remove(toyId) {
+    return storageService.remove(TOY_KEY, toyId)
+}
+
+function getEmptyToy(name = '', imgUrl = '', price = 100, labels = []) {
+    return {
+        name,
+        imgUrl,
+        price,
+        labels,
+        createdAt: new Date.now(),
+        isStock: true
+    }
+}
+
+function getDefaultFilter() {
+    return { txt: '', price: 0, labels: [] }
 }
