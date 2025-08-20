6fbfbb3f605859fd275ddd5d21fb5485c410cd0d
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-08-20 21:34:00 +0300

    Implement query

---
+++ src/services/toy.service.js
@@ -0,0 +1,25 @@
+import { storageService } from "./async-storage.service.js"
+
+const TOY_KEY = 'toyDB'
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyToy,
+    getDefaultFilter
+}
+
+function query(filterBy = {}) {
+    return storageService.query(TOY_KEY)
+        .then(toys => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                toys = toys.filter(toy => regExp.test(toy.txt))
+            }
+            return toys
+        })
+
+
+}
