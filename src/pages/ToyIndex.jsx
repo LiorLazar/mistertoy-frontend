7e14e35c99397e74195ffdc47656d06bac3cf18e
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-08-25 07:59:13 +0300

    Refactor Index

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -1,36 +1,68 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
-import { loadToys, setFilterBy } from "../services/store/actions/toy.actions.js"
-import { showErrorMsg } from "../services/event-bus.service.js"
-import { ToyFilter } from "../cmps/ToyFilter.jsx"
-import { ToyList } from "../cmps/ToyList.jsx"
+import { loadToys, setFilterBy } from "../services/store/actions/toy.actions"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
+import { ToyFilter } from "../cmps/ToyFilter"
+import { ToyList } from "../cmps/ToyList"
+import { Link } from "react-router-dom"
 
 export function ToyIndex() {
     const toys = useSelector(storeState => storeState.toyModule.toys)
     const filterBy = useSelector(storeState => storeState.toyModule.filterBy)
-    const isLoading = useSelector(storeState => storeState.toyModule.isLoading)
-
-    const allLabels = Array.from(new Set(toys.flatMap(toy => toy.labels)))
-    const labelOptions = allLabels.map(label => ({ value: label, label }))
+    const sortBy = useSelector(storeState => storeState.toyModule.sortBy)
+    const isLoading = useSelector(storeState => storeState.toyModule.flag.isLoading)
+    const toyLabels = useSelector(storeState => storeState.toyModule.toylabels)
 
     useEffect(() => {
         loadToys()
             .catch(err => {
+                console.log('err:', err)
                 showErrorMsg('Cannot load toys!')
             })
-    }, [filterBy])
+    }, [filterBy, sortBy])
+
+    function onRemoveToy(toyId) {
+        removeToy(toyId)
+            .then(() => showSuccessMsg('Toy Removed'))
+            .catch(err => {
+                console.log('Cannot remove toy', err)
+                showErrorMsg('Cannot remove toy')
+            })
+    }
 
     function onSetFilter(filterBy) {
         setFilterBy(filterBy)
     }
 
+    function onSetSort(sortBy) {
+        setSort(sortBy)
+    }
+
     return (
-        <div>
-            <h3>Toys App</h3>
-            <main>
-                <ToyFilter filterBy={filterBy} onSetFilter={onSetFilter} labels={labelOptions} />
-                {!isLoading ? <ToyList toys={toys} /> : <div>Loading...</div>}
-            </main>
-        </div>
+        <section className="toy-index">
+            <section className="toy-filter-sort container">
+                <ToyFilter
+                    filterBy={filterBy}
+                    onSetFilter={onSetFilter}
+                    toylabels={toyLabels}
+                />
+                <ToySort sortBy={sortBy} onSetSort={onSetSort} />
+
+                <div style={{ marginBlockStart: '0.5em', textAlign: 'center' }}>
+                    <button style={{ marginInline: 0 }}>
+                        <Link to="/toy/edit"></Link>
+                    </button>
+                </div>
+
+                {isLoading && <Loader />}
+                {!isLoading && <ToyList toys={toys} onRemoveToy={onRemoveToy} />}
+                <PopUp isOpen={filterBy.txt === 'xxx'}>
+                    <>
+                        <h1>Hello!</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cupiditate facilis quibusdam consectetur eius quis, suscipit veniam quam. Impedit veritatis eius ea sunt excepturi quia eveniet dolorum, culpa placeat natus.</p>
+                    </>
+                </PopUp>
+            </section>
+        </section>
     )
 }
