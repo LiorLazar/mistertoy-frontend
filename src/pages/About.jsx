2538cbddce3a731bfe1a8c902688f003594ef691
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-09-01 08:33:23 +0300

    Implement Google Maps Integration

---
+++ src/pages/About.jsx
@@ -0,0 +1,49 @@
+import { AdvancedMarker, APIProvider, InfoWindow, Map, Pin, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
+
+const API_KEY = 'AIzaSyBSKU4OO6xlyDxqn2rPgWc8gIiTYKmZtv0'
+
+export function About() {
+    const [coords, setCoords] = useState({ lat: 32.0853, lng: 34.7818 })
+    const [isInfoOpen, setIsInfoOpen] = useState(false)
+    const [markerRef, marker] = useAdvancedMarkerRef()
+
+    function handleMapClick(ev) {
+        const { latLng } = ev.detail
+        ev.map.panTo(latLng)
+        setCoords(latLng)
+    }
+    return (
+        <section className="google-map">
+            <h1>Google Maps!</h1>
+            <APIProvider apiKey={API_KEY}>
+                <Map
+                    defaultCenter={coords}
+                    defaultZoom={10}
+                    mapId="DEMO_MAP_ID"
+                    onClick={handleMapClick}
+                >
+                    <AdvancedMarker onClick={() => setIsInfoOpen(isOpen => !isOpen)} ref={markerRef} position={coords} >
+                        <Pin
+                            background={'#22ccff'}
+                            borderColor={'#1e89a1'}
+                            glyphColor={'#0f677a'}>
+
+                        </Pin>
+                        <img style={{ width: '5em' }} src="./logo.png" alt="" />
+                    </AdvancedMarker>
+                    {isInfoOpen &&
+                        <InfoWindow className='red-class' anchor={marker} maxWidth={200}>
+                            <div>
+                                This is the content for another infowindow with <em>HTML</em>
+                                -elements.
+                            </div>
+                            <pre>{JSON.stringify(coords, null, 4)}</pre>
+                        </InfoWindow>
+
+                    }
+                </Map>
+            </APIProvider>
+        </section>
+    )
+}
