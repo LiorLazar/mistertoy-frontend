f95fde3927d0f2922ea04999022ae37b63ee78f8
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-09-01 07:27:36 +0300

    Implement Formik

--- src/pages/ToyEdit.jsx
+++ src/pages/ToyEdit.jsx
@@ -1,11 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
+
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { toyService } from '../services/toy.service'
-import { useSelector } from 'react-redux'
 import { saveToy } from '../services/store/actions/toy.actions'
 import { useOnlineStatus } from '../hooks/useOnlineStatus'
 import { useConfirmTabClose } from '../hooks/useConfirmTabClose'
+import { TextField } from '@mui/material'
+
+const EditToySchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short')
+        .max(50, 'Too Long')
+        .required('Required'),
+    price: Yup.number()
+        .required('Required'),
+    labels: Yup.array()
+        .min(1, 'Select at least one label')
+        .of(Yup.string().required())
+        .required('Required')
+})
 
 export function ToyEdit() {
     const navigate = useNavigate()
@@ -67,72 +84,65 @@ export function ToyEdit() {
         required: true
     }
 
+    function CustomInput(props) {
+        return (
+            <TextField {...props} variant="standard" />
+        )
+    }
+
+    function formValidationClass(errors, touched) {
+        const isError = !!Object.keys(errors).length
+        const isTouched = !!Object.keys(touched).length
+        if (!isTouched) return ''
+        return isError ? 'error' : 'valid'
+    }
+
     // console.log('toyToEdit.labels:', toyToEdit.labels)
     return (
         <section className="toy-edit">
             <h2>{toyToEdit._id ? 'Edit' : 'Add'} Toy</h2>
-            <form onSubmit={onSaveToy}>
-                <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={toyToEdit.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="price">Price:</label>
-                    <input
-                        type="number"
-                        id="price"
-                        name="price"
-                        value={toyToEdit.price || ''}
-                        {...priceValidations}
-                        // min="1"
-                        // required
-                        onChange={handleChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="labels">Labels:</label>
-                    <select
-                        id="labels"
-                        name="labels"
-                        multiple
-                        value={toyToEdit.labels}
-                        onChange={handleChange}
-                    >
-                        {labels.map(label => (
-                            <option key={label} value={label}>
-                                {label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-
-                {toyToEdit._id && (
-                    <div className="form-group">
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="inStock"
-                                checked={toyToEdit.inStock}
+            <Formik
+                initialValues={{
+                    name: toyToEdit.name,
+                    price: toyToEdit.price,
+                    labels: toyToEdit.labels
+                }}
+                validationSchema={EditToySchema}
+                enableReinitialize={true}
+            >
+                {({ errors, touched, isValid, dirty, status }) => {
+                    const validationClass = formValidationClass(errors, touched)
+                    return (
+                        <Form className={`formik ${validationClass}`}>
+                            <Field as={CustomInput} label='name' name='name' onChange={handleChange} />
+                            {errors.name && touched.name && (
+                                <div className='errors'>{errors.name}</div>
+                            )}
+                            <Field as={CustomInput} label='price' name='price' type='number' onChange={handleChange} />
+                            {errors.price && touched.price && (
+                                <div className='errors'>{errors.price}</div>
+                            )}
+                            <Field
+                                as="select"
+                                name="labels"
+                                multiple
+                                value={toyToEdit.labels}
                                 onChange={handleChange}
-                            />
-                            In Stock
-                        </label>
-                    </div>
-                )}
+                            >
+                                {labels.map(label => (
+                                    <option key={label} value={label}>
+                                        {label}
+                                    </option>
+                                ))}
+                            </Field>
+                            {errors.labels && touched.labels && (
+                                <div className='errors'>{errors.labels}</div>
+                            )}
+                        </Form>
+                    )
+                }}
+            </Formik>
 
-                <button>
-                    {toyToEdit._id ? 'Update Toy' : 'Add'}
-                </button>
-            </form>
             <section>
                 <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>
             </section>
